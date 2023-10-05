4f8ea1134c604153d28a4b7e2b12e3d3dd8b1844
jorgito <jdasilv8@educacion.navarra.es>
2023-10-05 15:45:46 +0200

    Create Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,6 @@
+n = int(input("Introduce el primer valor: "))
+m = int(input("Introduce el segundo valor: "))
+c = n/m 
+r = n%m 
+
+print ("La división entre los valores intrucidos nos dan un cociente igual a",c,"y un resto igual a",r)
