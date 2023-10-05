a4cf72dffd983b2a1fbbaf36543353c27f61eb49
jorgito <jdasilv8@educacion.navarra.es>
2023-10-05 16:42:41 +0200

    Create Ejercicioo12.py

---
+++ Ejercicioo12.py
@@ -0,0 +1,9 @@
+panayer = 3.49 * 0.4
+ventapanayer = int(input("Cuántos panes que no son del día se vendieron?: "))
+
+x = panayer * ventapanayer
+x = round (x,2)
+
+print ("Precio barra de pan 3.49€")
+print ("El pan de ayer tiene un 60% de descuento")
+print ("Total a pagar:", x)
